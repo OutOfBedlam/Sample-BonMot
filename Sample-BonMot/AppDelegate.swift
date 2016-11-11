1ecaae1f75089133caaf91e9204446703e885fd5
Kwon, Yeong-Eon <happylogin@gmail.com>
2016-11-11 14:16:19 +0900

    Initial Commit

---
+++ Sample-BonMot/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sample-BonMot
+//
+//  Created by Eirny on 2016. 11. 11..
+//  Copyright © 2016년 OutOfBedlam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
